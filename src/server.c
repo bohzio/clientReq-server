f89f4df674cbe035c098b526aca6e3ffbf042657
mattia <mattia@banana.it>
2019-06-12 21:57:09 +0200

    final improvement

--- src/server.c
+++ src/server.c
@@ -18,9 +18,14 @@ char *basePathClientFifo = "/tmp/CLIENTFIFO.";
 char *fileFtokPath = "/tmp/fileFTOK";
 int serverFIFO, serverFIFO_extra;
 
-int totalEntries = 10;
+#define MAXENTRIES 100
+#define MUTEX 0
 
-int projid = 123;
+#define STAMPA 1
+#define SALVA 2
+#define INVIA 3
+
+int projectId = 123;
 
 struct SharedItem *entries;
 int shmid;
@@ -31,31 +36,31 @@ pid_t pid;
 /**
  * Function that check if the service is ok
  * @param service
- * @return 0 if is ok 1 if is wrong
+ * @return 0 ok, 1 wrong
  */
-int checkRequestService(char *service)
-{
-    if ((strncmp(service, "stampa", strlen("stampa")) == 0 || strncmp(service, "salva", strlen("salva")) == 0 || strncmp(service, "invia", strlen("invia")) == 0))
-    {
+int checkRequestService(char *service) {
+    if ((strncmp(service, "stampa", strlen("stampa")) == 0 || strncmp(service, "salva", strlen("salva")) == 0 ||
+         strncmp(service, "invia", strlen("invia")) == 0)) {
         return 0;
     }
     return 1;
 }
 
-
-void sigHandler()
-{
+/**
+ * Handler of the parent process
+ */
+void sigParentHandler() {
 
     // detach the shared memory segment
     printf("<Server> detaching the shared memory segment...\n");
     free_shared_memory(entries);
 
     // remove the shared memory segment
-    printf("<Server> removing the shared memory segment[%d]...\n",shmid);
+    printf("<Server> removing the shared memory segment[%d]...\n", shmid);
     remove_shared_memory(shmid);
 
     printf("<Server> remove file ftok\n");
-    if (unlink(fileFtokPath) == -1){
+    if (unlink(fileFtokPath) == -1) {
         errExit("Remove of file ftok failed");
     }
 
@@ -66,8 +71,33 @@ void sigHandler()
 
 }
 
-void sendResponse(struct Request *request)
-{
+
+int genereteKeyService(int timestamp,char *service){
+
+    int key = timestamp % 10000; //get the last 4 digits
+    key = (key + rand() % 1000 + 1); //sum up a random number
+
+    int serviceId = -1;
+
+    if (strcmp(service, "stampa") == 0) {
+        serviceId = STAMPA;
+    } else if (strcmp(service, "salva") == 0) {
+        serviceId = SALVA;
+    } else if (strcmp(service, "invia") == 0) {
+        serviceId = INVIA;
+    }
+
+    int i=1;
+    while (i<=key){
+        i*=10;
+    }
+
+
+
+    return serviceId * i + key;
+}
+
+void sendResponse(struct Request *request) {
 
     int cont = 0;
     char pathClientFifo[25];
@@ -75,8 +105,7 @@ void sendResponse(struct Request *request)
 
     printf("%s\n", request->idUser);
 
-    if (checkRequestService(request->service) == 0)
-    {
+    if (checkRequestService(request->service) == 0) {
         // Step-2: The client opens the server's FIFO to send a Request
         printf("<Server> opening FIFO %s...\n", pathClientFifo);
         int clientFIFO = open(pathClientFifo, O_WRONLY);
@@ -84,12 +113,10 @@ void sendResponse(struct Request *request)
             errExit("open failed");
 
 
-        semOp(semid, 0 , -1);
+        semOp(semid, MUTEX, -1);
 
-        for (int i = 0; i < totalEntries; i++)
-        {
-            if (entries[i].idUser[0] == 0)
-            {
+        for (int i = 0; i < MAXENTRIES; i++) {
+            if (entries[i].idUser[0] == 0) {
                 //E' vuoto o è stato cancellato da processo keyserver
                 //Setto il contatore a i così poi verrà scritto in quell'elemento
                 cont = i;
@@ -102,30 +129,15 @@ void sendResponse(struct Request *request)
         strcpy(entries[cont].idUser, request->idUser);
         entries[cont].timestamp = timeStamp;
 
-        int stampa = 3;
-        int invia = 7;
-        int salva = 11;
-        int key = 0;
+        int key = genereteKeyService(timeStamp, request->service);
 
-        if (strcmp(request->service, "stampa") == 0)
-        {
-            key = timeStamp + stampa;
-        }
-        else if (strcmp(request->service, "salva") == 0)
-        {
-            key = timeStamp + salva;
-        }
-        else if (strcmp(request->service, "invia") == 0)
-        {
-            key = timeStamp + invia;
-        }
 
         struct Response response;
 
         entries[cont].key = key;
         response.key = key;
 
-        semOp(semid, 0 , 1);
+        semOp(semid, MUTEX, 1);
 
         // Step-3: The Server sends a Response through the client's FIFO
         printf("<Server> sending ... \n");
@@ -136,61 +148,64 @@ void sendResponse(struct Request *request)
 }
 
 
-void setSignalHandler(){
-    // set of signals (N.B. it is not initialized!)
+void setSignalHandler() {
+    // set of signals not initialized
     sigset_t mySet;
     // initialize mySet to contain all signals
     sigfillset(&mySet);
-    // remove SITERM from mySet
+    // remove SIGTERM from mySet
     sigdelset(&mySet, SIGTERM);
     // blocking all signals but SIGTERM
     sigprocmask(SIG_SETMASK, &mySet, NULL);
 
-    // set the function sigHandler as handler for the signal SIGINT
-    if (signal(SIGTERM, sigHandler) == SIG_ERR)
+    // set the function sigParentHandler as handler for the signal SIGTERM
+    if (signal(SIGTERM, sigParentHandler) == SIG_ERR)
         errExit("change signal handler failed");
 }
 
 
-
-key_t getKey(){
-    open(fileFtokPath,O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-
-    return ftok(fileFtokPath,projid);
+/**
+ * Function that return a key with ftok
+ * @return key_t key
+ */
+key_t getKey() {
+    open(fileFtokPath, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+    return ftok(fileFtokPath, projectId);
 }
 
-
-void sigHandlerChild(){
+/**
+ * Signal handler of the child
+ */
+void sigHandlerChild() {
     printf("<Subprocess child> Killed\n");
     exit(0);
 }
 
-void child(){
-    if(signal(SIGTERM, sigHandlerChild) == SIG_ERR)
+
+/**
+ * Function that contains the code of the child
+ */
+void child() {
+
+    if (signal(SIGTERM, sigHandlerChild) == SIG_ERR)
         errExit("change signal handler child failed\n");
 
 
-    while (1)
-    {
+    while (1) {
         sleep(30);
         printf("<Subprocess child> checking shared memory\n");
         fflush(stdout); //todo studiare come funziona il fflush di preciso
 
 
-        semOp(semid, 0 , -1);
+        semOp(semid, MUTEX, -1);
         time_t now = time(0);
-        for (int i = 0; i < totalEntries; i++)
-        {
+        for (int i = 0; i < MAXENTRIES; i++) {
 
-            if (entries[i].idUser[0] != 0)
-            {
+            if (entries[i].idUser[0] != 0) {
 
                 double timeDiff = difftime(now, entries[i].timestamp);
-                if (timeDiff >= 60)
-                { // 5 minuti = 60 * 5, metto 1 minuto se non non mi passa più
-                    //devo eliminare la entry
-                    //settando il primo byte a 0 non viene più riconosciuta come una stringa
-                    //e posso facilmente identificarla come cancellata
+
+                if (timeDiff >= 60) {  //todo put 60 * 5 in production
                     entries[i].idUser[0] = 0;
                     entries[i].key = 0;
                     entries[i].timestamp = 0;
@@ -199,12 +214,12 @@ void child(){
                 printf("%s,%d,%ld\n", entries[i].idUser, entries[i].key, entries[i].timestamp);
             }
         }
-        semOp(semid, 0 ,1);
+        semOp(semid, MUTEX, 1);
     }
 }
 
 
-void openFIFO(){
+void openFIFO() {
     printf("<Server> Making FIFO...\n");
     // make a FIFO with the following permissions:
     // user:  read, write
@@ -228,8 +243,27 @@ void openFIFO(){
 }
 
 
-int main(int argc, char *argv[])
-{
+int createSemaphoreSet(key_t key) {
+    semid = semget(key, 1, IPC_CREAT | S_IRUSR | S_IWUSR);
+    if (semid == -1)
+        errExit("semget failed");
+
+
+    union semun arg;
+    //set semaphore value to 1
+    arg.val = 1;
+
+    //initialize the 0-th semaphore to 1
+    if (semctl(semid, 0, SETVAL, arg) == -1)
+        errExit("semctl SETVAL failed");
+
+    return semid;
+}
+
+
+int main(int argc, char *argv[]) {
+
+    srand(time(0));
 
     printf("Hi, I'm Server program!\n");
 
@@ -237,34 +271,20 @@ int main(int argc, char *argv[])
 
     key_t key = getKey();
 
-    // creo segmento memoria condiviso
     printf("<Server> allocating a shared memory segment\n");
 
-    shmid = alloc_shared_memory(key, sizeof(struct SharedItem *) * totalEntries); //alloco spazio per 10 SharedItem
-    //me lo prendo
-    entries = (struct SharedItem *)get_shared_memory(shmid, 0);
+    shmid = alloc_shared_memory(key, sizeof(struct SharedItem *) * MAXENTRIES);
 
+    entries = (struct SharedItem *) get_shared_memory(shmid, 0);
 
-    semid = semget(key, 1,IPC_CREAT |  S_IRUSR | S_IWUSR);
-    if(semid == -1)
-        errExit("semget failed");
-
-    // Initialize the semaphore set
-    unsigned short semInitVal[] = {1};
-    union semun arg;
-    arg.array = semInitVal;
-
-    if (semctl(semid, 0 /*ignored*/, SETALL, arg) == -1)
-        errExit("semctl SETALL failed");
-
+    semid = createSemaphoreSet(key);
 
 
     //lancio processo figlio keymanager
     pid = fork();
     if (pid == -1)
         printf("subprocess keymanager not created\n");
-    else if (pid == 0)
-    {
+    else if (pid == 0) {
         printf("<Server> creates child\n");
         child();
     }
@@ -275,21 +295,17 @@ int main(int argc, char *argv[])
     int bR = -1;
 
     struct Request request;
-    do
-    {
+    do {
         printf("<Server> waiting for a Request...\n");
         // Read a request from the FIFO
         bR = read(serverFIFO, &request, sizeof(struct Request));
 
         // Check the number of bytes read from the FIFO
-        if (bR == -1)
-        {
+        if (bR == -1) {
             printf("<Server> it looks like the FIFO is broken\n");
-        }
-        else if (bR != sizeof(struct Request) || bR == 0)
+        } else if (bR != sizeof(struct Request) || bR == 0)
             printf("<Server> it looks like I did not receive a valid request\n");
-        else
-        {
+        else {
             sendResponse(&request);
         }
 
